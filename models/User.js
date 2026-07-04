6ceac8606ab15525bfa3b2c1e06015574de603bd
Alirez-13 <alirezaholghi@gmail.com>
2026-07-04 10:12:40 +0330

    Initi repo & Added User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './data/database.sqlite' 
+});
+
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  family: {
+    type: DataTypes.STRING,
+    allowNull: false
+  } ,
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true 
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  PhoneNo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW
+  }
+});
+
+module { sequelize, User };
